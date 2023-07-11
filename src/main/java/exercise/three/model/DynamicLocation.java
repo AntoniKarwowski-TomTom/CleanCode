19a496332d41657b6f8303225f0f716995bffbf8
Kacper Filutowski <kacper.filutowski@tomtom.com>
2023-07-11 10:26:06 +0200

    Exercise three

---
+++ src/main/java/exercise/three/model/DynamicLocation.java
@@ -0,0 +1,16 @@
+package exercise.three.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+//Stub class
+public class DynamicLocation {
+    public String getPoiId(){
+        return "";
+    }
+
+    public Set<String> getEvseIds(){
+        return new HashSet<>();
+    }
+}
---
+++ src/main/java/exercise/three/model/EppPoi.java
@@ -0,0 +1,14 @@
+package exercise.three.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//Stub class
+public class EppPoi {
+    public String getLastProcessingTime(){
+        return "";
+    }
+    public Set<String> getEvseIds(){
+        return new HashSet<>();
+    }
+}
